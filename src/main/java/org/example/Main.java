9b5288fd65ea0b76ec853fc1c67157dd7e00dfa2
davidscavnicky <davidscava@gmail.com>
2022-12-06 18:40:31 +0100

    the elevator will be randomly located at the originFloor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,46 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello to the elevator app!");
-        new Elevator();
+        new Elevator().runElevator();
     }
 }
+// the app calls elevator from random position to the
+// customer and thereafter sends the lift to desired location
 
 class Elevator {
-    public int floor;
+//    public int floor = 0;
+    public static int maxFloor = getRandomNumberInRange(4,10);
+    public static int originFloor;
+
+    public void runElevator() {
+        originFloor = getRandomNumberInRange(0, 10);
+
+        System.out.println("The generated building has " + maxFloor + " floors.");
+        System.out.println("Please, write your floor's number: ");
+        int currentFloor = readFloorInteger();
+    }
+
+
+    private static int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    private int readFloorInteger() {
+        Scanner keyboard = new Scanner(System.in);
+        return keyboard.nextInt();
+    }
+
+
 
 }
 
