11406c764e7849708f030d330c65f7f4298c7597
davidscavnicky <davidscava@gmail.com>
2022-12-07 15:23:26 +0100

    move up, move down, movement deleted + function that prints floor number and sleeps addded

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
     }
 }
 // the app calls elevator from random position to the
-// customer and thereafter sends the lift to the desired location
+// customer and thereafter sends the lift to the customer's desired location
 
 class Elevator {
 //    public int floor = 0;
@@ -41,24 +41,6 @@ class Elevator {
         System.out.println("You have arrived at your destination.");
     }
 
-    private void movement(int currentFloor) {
-        if (currentFloor < originFloor) {
-            moveDown(currentFloor);
-        }else {
-            moveUp(currentFloor);
-        }
-    }
-    private void moveUp(int currentFloor){
-        while (originFloor < currentFloor) {
-            originFloor++;
-            System.out.println(originFloor);
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
     private void movementOfTheElevator(int currentFloor) {
 //        direction = directionChoice(currentFloor);
         switch(directionChoice(currentFloor)) {
@@ -66,14 +48,16 @@ class Elevator {
                 System.out.println("The elevator is on the way UP.");
                 while (originFloor < currentFloor) {
                     originFloor++;
-                    System.out.println(originFloor);
+//                    System.out.println(originFloor);
+                    printAndSleep(originFloor);
                 }
                 break;
             case DOWN:
                 System.out.println("The elevator is on the way DOWN.");
                 while (originFloor > currentFloor) {
                     originFloor--;
-                    System.out.println(originFloor);
+//                    System.out.println(originFloor);
+                    printAndSleep(originFloor);
                 }
         }
     }
@@ -86,16 +70,12 @@ class Elevator {
         }
     }
 
-
-    private void moveDown(int currentFloor){
-        while (originFloor > currentFloor) {
-            originFloor--;
-            System.out.println(originFloor);
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+    private void printAndSleep(int floorNumber) {
+        System.out.println(floorNumber);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -103,22 +83,9 @@ class Elevator {
         if (min >= max) {
             throw new IllegalArgumentException("max must be greater than min");
         }
-
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
     }
-
-//    private int readFloorInteger() {
-//        int someInput = 0;
-//        try {
-//            Scanner scanner = new Scanner(System.in);
-//            someInput = scanner.nextInt();
-//        } catch (Exception e) {
-//            System.out.println("The input value is not a valid integer.");
-//            System.exit(0);
-//        }
-//        return someInput;
-//    }
 
     private int readFloorInteger() {
         String someInput = null;
